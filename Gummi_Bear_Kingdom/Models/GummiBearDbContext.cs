479f95012e515f5ded46fce8ed4069c4eca60da9
Patrick Napper <patrick@patricknapper.com>
2018-01-05 09:19:44 -0800

    add product model and DbContext file.

---
+++ Gummi_Bear_Kingdom/Models/GummiBearDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gummi_Bear_Kingdom.Models
+{
+    public class GummiBearDbContext : DbContext
+    {
+        public GummiBearDbContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=Gummi_Bear_Kingdom;uid=root;pwd=root;");
+        }
+
+        public GummiBearDbContext(DbContextOptions<GummiBearDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Gummi_Bear_Kingdom/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gummi_Bear_Kingdom.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Description { get; set; }
+    }
+}
